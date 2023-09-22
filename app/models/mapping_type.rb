156b033c3d2204c3de028ccca0c1a6f9e1f9bd07
Tom Lehman <tom.s.lehman@gmail.com>
2023-09-22 15:03:43 -0400

    remove default values before comparing mapping proxies

--- app/models/mapping_type.rb
+++ app/models/mapping_type.rb
@@ -4,15 +4,25 @@ class MappingType < TypedVariable
   end
   
   def serialize
-    value.data.each.with_object({}) do |(key, value), h|
-      next if value.value == value.type.default_value
-
+    value.data_defaults_pruned.each.with_object({}) do |(key, value), h|
       h[key.serialize] = value.serialize
     end
   end
   
   class Proxy
     attr_accessor :key_type, :value_type, :data
+    
+    def ==(other)
+      return false unless other.is_a?(self.class)
+      
+      other.key_type == key_type &&
+      other.value_type == value_type &&
+      other.data_defaults_pruned == data_defaults_pruned
+    end
+    
+    def data_defaults_pruned
+      data.reject { |key, value| value.value == value.type.default_value }
+    end
     
     def initialize(initial_value = {}, key_type:, value_type:)
       self.key_type = key_type
