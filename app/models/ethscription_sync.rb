b581e1f2577ef734c4ae3e670ccc200e3b1e860e
Tom Lehman <tom.s.lehman@gmail.com>
2023-09-25 17:30:43 -0400

    stop always deleting latest ethscription in import

--- app/models/ethscription_sync.rb
+++ app/models/ethscription_sync.rb
@@ -51,7 +51,18 @@ class EthscriptionSync
         ethscription_id: ethscriptions.first[:ethscription_id]
       )
       
-      starting_ethscription&.delete_with_later_ethscriptions
+      server_blockhash = ethscriptions.first[:block_blockhash]
+      our_blockhash = starting_ethscription&.block_blockhash
+      
+      Ethscription.transaction do
+        starting_ethscription&.later_ethscriptions&.delete_all
+        
+        if server_blockhash != our_blockhash
+          starting_ethscription&.delete
+        else
+          ethscriptions.shift
+        end
+      end
       
       sorted_response = ethscriptions.sort_by do |e|
         [e[:block_number], e[:transaction_index]]
