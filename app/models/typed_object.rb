30b9b17e539fd0ffd91455f2e77ace3c2d654eda
Tom Lehman <tom.s.lehman@gmail.com>
2023-10-28 10:44:21 -0400

    whitespace

--- app/models/typed_object.rb
+++ app/models/typed_object.rb
@@ -1,19 +1,18 @@
-  module TypedObject
-    def on_change
-    end
-    
-    def on_change=(*)
-    end
-  
-    def serialize
-      self
-    end
-  
-    def deserialize(*)
-    end
-  
-    def value
-      self
-    end
+module TypedObject
+  def on_change
   end
   
+  def on_change=(*)
+  end
+
+  def serialize
+    self
+  end
+
+  def deserialize(*)
+  end
+
+  def value
+    self
+  end
+end
