e13e0afca9aa1c80dcb219b97fe152261418a95a
Michael Hirsch <michael@hirsch.me>
2023-12-27 00:43:34 -0600

    fix issue with transaction cache

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -26,7 +26,7 @@ class TransactionsController < ApplicationController
       )
     end
     
-    cache_key = ["transactions_index", EthBlock.max_processed_block_hash, page, per_page]
+    cache_key = ["transactions_index", EthBlock.max_processed_block_number, page, per_page]
   
     result = Rails.cache.fetch(cache_key) do
       res = scope.page(page).per(per_page).to_a
