a57316365f3b5ea54e0ca45c430417fd839a8c68
Tom Lehman <tom.s.lehman@gmail.com>
2024-01-04 11:49:11 -0500

    Checks skeleton

---
+++ spec/models/checks_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'Checks' do
+  let(:user_address) { "0xc2172a6315c1d7f6855768f843c420ebb36eda97" }
+  
+  before(:all) do
+    update_supported_contracts(
+      'Checks',
+    )
+  end
+  
+  it 'does it' do
+    dep = trigger_contract_interaction_and_expect_success(
+      from: user_address,
+      payload: {
+        to: nil,
+        data: {
+          type: "Checks",
+          args: {
+            name: "Checks",
+            symbol: "CHK",
+          }
+        }
+      }
+    )
+    
+    trigger_contract_interaction_and_expect_success(
+      from: user_address,
+      payload: {
+        to: dep.address,
+        data: {
+          function: "mint",
+          args: 1
+        }
+      }
+    )
+    
+    token_uri = ContractTransaction.make_static_call(
+      contract: dep.address, 
+      function_name: "tokenURI", 
+      function_args: { id: "0" }
+    )
+    
+    # puts JSON.parse(token_uri[/.*?,(.*)/, 1])['image']
+  end
+
+end
