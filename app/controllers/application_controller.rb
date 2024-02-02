33ef2c1f72f8758662cbc7910ced4a5ec6799e08
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-01 19:24:41 -0500

    Enable multi-db setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_all_requests_if_required
+  around_action :use_read_only_database_if_available
+  
   private
   
   def convert_int_to_string(result)
@@ -13,6 +16,35 @@ class ApplicationController < ActionController::API
       result.map { |value| convert_int_to_string(value) }
     else
       result
+    end
+  end
+  
+  def authorized?
+    authorization_header = request.headers['Authorization']
+    return false if authorization_header.blank?
+  
+    token = authorization_header.remove('Bearer ').strip
+    stored_tokens = JSON.parse(ENV.fetch('API_AUTH_TOKENS', "[]"))
+    
+    stored_tokens.include?(token)
+  rescue JSON::ParserError
+    Airbrake.notify("Invalid API_AUTH_TOKEN format: #{ENV.fetch('API_AUTH_TOKENS', "[]")}")
+    false
+  end
+  
+  def authorize_all_requests_if_required
+    if ENV['REQUIRE_AUTHORIZATION'].present? && ENV['REQUIRE_AUTHORIZATION'] != 'false'
+      unless authorized?
+        render json: { error: "Unauthorized" }, status: :unauthorized
+      end
+    end
+  end
+  
+  def use_read_only_database_if_available
+    if ENV['FOLLOWER_DATABASE_URL'].present?
+      ActiveRecord::Base.connected_to(role: :reading) { yield }
+    else
+      yield
     end
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  
+  if ENV['FOLLOWER_DATABASE_URL'].present?
+    connects_to database: { writing: :primary, reading: :primary_replica }
+  end
 end
