fb12566b8704e3a45f1b9587be9b08ec84dc49e4
Tom Lehman <tom.s.lehman@gmail.com>
2024-04-04 15:21:47 -0400

    Update blocks endpoint

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,67 +1,41 @@
 class BlocksController < ApplicationController
   cache_actions_on_block
+  before_action :set_eth_block_scope
 
   def index
-    page = (params[:page] || 1).to_i
-    per_page = (params[:per_page] || 50).to_i
-    per_page = 50 if per_page > 50
+    results, pagination_response = paginate(@eth_block_scope)
     
-    scope = if system_start_block.present?
-      EthBlock.where("block_number >= ?", system_start_block).
-        processed.
-        order(block_number: :desc)
-    else
-      EthBlock.none
-    end
-    
-    cache_key = ["blocks_index", scope, page, per_page]
-  
-    result = Rails.cache.fetch(cache_key) do
-      res = scope.page(page).per(per_page).to_a
-      numbers_to_strings(res)
-    end
-  
     render json: {
-      result: result
+      result: numbers_to_strings(results),
+      pagination: pagination_response
     }
   end
 
   def show
-    scope = if system_start_block.present?
-      EthBlock.where("block_number >= ?", system_start_block).
-        processed.
-        where(block_number: params[:id])
-    else
-      EthBlock.none
-    end
-    
-    eth_block = scope.first
+    eth_block = @eth_block_scope.where(block_number: params[:id]).first
 
-    if eth_block.blank?
-      render json: { error: "Block not found" }, status: 404
-      return
-    end
+    raise RequestedRecordNotFound unless eth_block
 
-    render json: {
-      result: numbers_to_strings(eth_block)
-    }
+    render json: { result: numbers_to_strings(eth_block) }
   end
 
   def total
-    total_blocks = if system_start_block.present?
-      EthBlock.processed.where("block_number >= ?", system_start_block).count
-    else
-      0
-    end
+    total_blocks = @eth_block_scope.count
     
-    render json: {
-      result: numbers_to_strings(total_blocks)
-    }
+    render json: { result: numbers_to_strings(total_blocks) }
   end
   
   private
   
+  def set_eth_block_scope
+    @eth_block_scope = if system_start_block.present?
+                         EthBlock.processed.where("block_number >= ?", system_start_block)
+                       else
+                         EthBlock.none
+                       end
+  end
+
   def system_start_block
-    SystemConfigVersion.current.start_block_number
+    @_system_start_block ||= SystemConfigVersion.current.start_block_number
   end
 end
