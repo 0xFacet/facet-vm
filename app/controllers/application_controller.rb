26dea85745746ba43b79bd3386e1db7a1d4382bb
Tom Lehman <tom.s.lehman@gmail.com>
2024-04-08 16:27:02 -0400

    Namespace v2 API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
   include FacetRailsCommon::ApplicationControllerMethods
+  
+  def api_version
+    params.fetch(:api_version, '1')
+  end
+  
+  def cursor_mode?
+    !!(
+      api_version.to_i >= 2 ||
+      params[:user_cursor_pagination] ||
+      params[:use_cursor_pagination] ||
+      params[:page_key]
+    )
+  end
 end
